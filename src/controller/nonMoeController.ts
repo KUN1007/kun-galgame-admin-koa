ce7b77a8fc0b935c0a89270e31385aa587e37827
KUN1007 <yuyuyukunkunkun@gmail.com>
2024-04-25 23:33:25 +0800

    feat: create non-moe logs

--- src/controller/nonMoeController.ts
+++ src/controller/nonMoeController.ts
@@ -1,11 +1,19 @@
 import { type Context } from 'koa'
 import NonMoeService from '@/service/nonMoeService'
+import AdminInfoService from '@/service/adminInfoService'
 
 class NonMoeController {
   async createNonMoeLog(ctx: Context) {
     const { uid, name, description, time, result } = ctx.request.body
 
     await NonMoeService.createNonMoeLog(uid, name, description, time, result)
+
+    const user = ctx.state.user
+    await AdminInfoService.createAdminInfo(
+      user.uid,
+      'post',
+      `${user.name} created a non-moe log\nuid: ${uid}\nname: ${name}\ndescription: ${description}\nresult: ${result}`
+    )
   }
 
   async getNonMoeLogsByUid(ctx: Context): Promise<void> {
@@ -21,12 +29,26 @@ class NonMoeController {
     const { nid } = ctx.params
     const result = ctx.request.body
     await NonMoeService.updateNonMoeLog(parseInt(nid), result)
+
+    const user = ctx.state.user
+    await AdminInfoService.createAdminInfo(
+      user.uid,
+      'update',
+      `${user.name} created a non-moe log\nresult: ${result}`
+    )
   }
 
   async withdrawNonMoeLog(ctx: Context) {
     const { nid } = ctx.params
 
     await NonMoeService.withdrawNonMoeLog(parseInt(nid))
+
+    const user = ctx.state.user
+    await AdminInfoService.createAdminInfo(
+      user.uid,
+      'delete',
+      `${user.name} deleted a non-moe log\nnid: ${nid}`
+    )
   }
 }
 
